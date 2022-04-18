59cacaf4be88f4595ec3426c4d636745114705fd
PyromaniacSquid <68219629+PyromaniacSquid@users.noreply.github.com>
2022-04-18 21:09:05 +0300

    Init

---
+++ Machine/Machine.cpp
@@ -0,0 +1,10 @@
+﻿// Machine.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
